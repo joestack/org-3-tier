858d4c1bfa7f149343744b3955c6b0065c611627
joestack <joern@joern-online.de>
2019-01-26 17:46:56 +0100

    add variable network address space

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "name" {}
-variable "network_address_space" {}
+variable "network_address_space" {
+          default = "192.168.0.0/16"
+}
 variable "environment_tag" {}
 variable "ttl" {}
 variable "owner" {}
