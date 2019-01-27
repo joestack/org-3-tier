aa678882d32e5c0a70b46f294a8a87d5d96b0c35
joestack <joern@joern-online.de>
2019-01-27 09:32:45 +0100

    new remote backend 2

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "sec_group" {
   backend = "remote"
   config {
     hostname   = "app.terraform.io"
-    organisation = "JoeStack"
+    organization = "JoeStack"
 
     workspaces {
       name = "org-3tier-sec"
